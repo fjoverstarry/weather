897682ed7466f9a4989b2d22e886337e8122323a
_go <jm1179356055@gmail.com>
2020-09-06 16:41:19 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -5,7 +5,7 @@ set -eux
 CITY=quanzhou
 LANGUAGE="zh-CN"
 UNIT=m
-UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
