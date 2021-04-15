86d7cb3d64e9f5a59548b41941119216fe150694
ming_jiang <jm1179356055@gmail.com>
2021-04-15 16:05:46 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-CITY=quanzhou
+CITY=xiamen
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
